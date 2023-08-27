68e96f3f8590e9b9fbe953ddafad41a5d8e15a2a
chuch <chuchoislas01@hotmail.com>
2023-08-27 17:25:31 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumatoriaPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculando Pi mediante la sumatoria de Leibniz");
+
+            int terminos = 1000000; // Cantidad de términos en la sumatoria
+            double piAproximado = CalcularPiLeibniz(terminos);
+
+            Console.WriteLine($"Valor aproximado de Pi: {piAproximado}");
+        }
+
+        static double CalcularPiLeibniz(int n)
+        {
+            double piAproximado = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double termino = 1.0 / (2 * i + 1);
+
+                // Alternamos la suma entre positivo y negativo según el término
+                if (i % 2 == 0)
+                {
+                    piAproximado += termino;
+                }
+                else
+                {
+                    piAproximado -= termino;
+                }
+
+            }
+
+            // Multiplicamos por 4 para obtener una aproximación de Pi
+            piAproximado *= 4;
+
+            return piAproximado;
+
+        }
+
+    }
+
+}
